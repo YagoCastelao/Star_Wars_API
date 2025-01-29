87664469a0881332159f253af20e5f74dcebd897
Yago Cima Castelao <yago_castelau@hotmail.com>
2025-01-29 04:15:02 +0100

    Proyecto Finalizado

---
+++ app.py
@@ -0,0 +1,119 @@
+from flask import Flask, jsonify, request
+from models import db, User, Planet, People, Favorite
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///starwars.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def home():
+    return "Bem-vindo ao StarWars Blog!"
+
+
+@app.route('/people', methods=['GET'])
+def list_people():
+    people = People.query.all()
+    return jsonify([{'id': p.id, 'name': p.name} for p in people])
+
+
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    person = People.query.get(people_id)
+    if person:
+        return jsonify({'id': person.id, 'name': person.name})
+    return jsonify({'error': 'Person not found'}), 404
+
+
+
+@app.route('/planets', methods=['GET'])
+def list_planets():
+    planets = Planet.query.all()
+    return jsonify([{'id': p.id, 'name': p.name} for p in planets])
+
+
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet:
+        return jsonify({'id': planet.id, 'name': planet.name})
+    return jsonify({'error': 'Planet not found'}), 404
+
+
+
+@app.route('/users', methods=['GET'])
+def list_users():
+    users = User.query.all()
+    return jsonify([{'id': u.id, 'username': u.username} for u in users])
+
+
+
+@app.route('/users/favorites', methods=['GET'])
+def list_favorites():
+    user = User.query.get(1)
+    if user:
+        favorites = Favorite.query.filter_by(user_id=user.id).all()
+        return jsonify([{'id': f.id, 'planet_id': f.planet_id, 'people_id': f.people_id} for f in favorites])
+    return jsonify({'error': 'User not found'}), 404
+
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user = User.query.get(1)
+    if user:
+        favorite = Favorite(user_id=user.id, planet_id=planet_id)
+        db.session.add(favorite)
+        db.session.commit()
+        return jsonify({'message': 'Planet added to favorites'}), 201
+    return jsonify({'error': 'User not found'}), 404
+
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST'])
+def add_favorite_people(people_id):
+    user = User.query.get(1)
+    if user:
+        favorite = Favorite(user_id=user.id, people_id=people_id)
+        db.session.add(favorite)
+        db.session.commit()
+        return jsonify({'message': 'People added to favorites'}), 201
+    return jsonify({'error': 'User not found'}), 404
+
+
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['DELETE'])
+def delete_favorite_planet(planet_id):
+    user = User.query.get(1)
+    if user:
+        favorite = Favorite.query.filter_by(user_id=user.id, planet_id=planet_id).first()
+        if favorite:
+            db.session.delete(favorite)
+            db.session.commit()
+            return jsonify({'message': 'Planet removed from favorites'}), 200
+        return jsonify({'error': 'Favorite not found'}), 404
+    return jsonify({'error': 'User not found'}), 404
+
+
+
+@app.route('/favorite/people/<int:people_id>', methods=['DELETE'])
+def delete_favorite_people(people_id):
+    user = User.query.get(1)
+    if user:
+        favorite = Favorite.query.filter_by(user_id=user.id, people_id=people_id).first()
+        if favorite:
+            db.session.delete(favorite)
+            db.session.commit()
+            return jsonify({'message': 'People removed from favorites'}), 200
+        return jsonify({'error': 'Favorite not found'}), 404
+    return jsonify({'error': 'User not found'}), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,26 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    favorites = db.relationship('Favorite', backref='user', lazy=True)
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    climate = db.Column(db.String(120))
+    population = db.Column(db.String(120))
+
+class People(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    height = db.Column(db.String(120))
+    mass = db.Column(db.String(120))
+
+class Favorite(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'))
+    people_id = db.Column(db.Integer, db.ForeignKey('people.id'))
